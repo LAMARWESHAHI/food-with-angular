d96d63e74677fe714d97acdfece892cff169e452
LAMRWESHAHI <lamar@example.com>
2024-10-05 09:57:46 +0300

    loop images

---
+++ src/app/services/food/food.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+  getAll(): string[] {
+    return [
+      '/Users/lamarweshahi/foodWebsite/src/assets/images/pexels-dana-tentis-118658-262959.jpg',
+      '/assets/images/pexels-elevate-1267320.jpg',
+      '/assets/images/pexels-janetrangdoan-1128678.jpg',
+      '/assets/images/pexels-sydney-troxell-223521-708488.jpg',
+      '/assets/images/pexels-sydney-troxell-223521-718742.jpg',
+      '/assets/images/pexels-vanmalidate-769289.jpg'
+    ]
+  }
+
+  constructor() { }
+}
