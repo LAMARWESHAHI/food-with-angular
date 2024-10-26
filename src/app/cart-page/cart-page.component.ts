c49087b1f610050004d1a0224a5036e399748029
LAMRWESHAHI <lamar@example.com>
2024-10-26 11:11:10 +0300

    Creating cart-page component

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CartService } from '../services/cart/cart.service';
+import { Cart } from '../shared/models/Cart';
+import { CartItem } from '../shared/CartItem';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent {
+  cart!: Cart;
+  constructor(private cartSerive: CartService) { }
 
+  removeFromCart(cartItem: CartItem) {
+    this.cartSerive.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    this.cartSerive.changeQuantity(cartItem.food.id, quantity);
+    this.setCart();
+  }
+
+  setCart() {
+    this.cart = this.cartSerive.getCart();
+  }
 }
